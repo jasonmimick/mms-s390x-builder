1d7fd555a28ff2e8c646b12064d461b3b57519de
Jason Mimick <jmimick@gmail.com>
2018-01-26 10:55:26 -0500

    make upload-builds detect os

--- upload-builds.sh
+++ upload-builds.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-s3cmd put ~/mms-monitoring/build/build/mongodb-mms-monitoring-agent-6.4.0.1-1.ubuntu_16.04_s390x.tar.gz s3://mms-agents-s390x/ubuntu/
-s3cmd put ~/mms-automation/build/build/mongodb-mms-automation-agent-4.9.0.1-1.ubuntu_16.04_s390x.tar.gz s3://mms-agents-s390x/ubuntu/
-s3cmd put ~/mms-backup/build/build/mongodb-mms-backup-agent-6.4.0.1-1.ubuntu_16.04_s390x.tar.gz s3://mms-agents-s390x/ubuntu/
+source set-os-version.sh
+PREFIX=$1
+echo "Uploading PREFIX=${PREFIX} OS_VERSION=${OS_VERSION}"
+
+s3cmd put ~/mms-monitoring/build/build/mongodb-mms-monitoring-agent-6.4.0.1-1.${OS_VERSION}_s390x.tar.gz s3://mms-agents-s390x/${PREFIX}/
+s3cmd put ~/mms-automation/build/build/mongodb-mms-automation-agent-4.9.0.1-1.${OS_VERSION}_s390x.tar.gz s3://mms-agents-s390x/${PREFIX}/
+s3cmd put ~/mms-backup/build/build/mongodb-mms-backup-agent-6.4.0.1-1.${OS_VERSION}_s390x.tar.gz s3://mms-agents-s390x/${PREFIX}/
