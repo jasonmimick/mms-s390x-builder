17a3cad624aa4b7d87ebd24ac68be79435150544
Jason Mimick <jmimick@gmail.com>
2018-01-27 10:48:00 -0500

    fixes to redhat demo config

--- testing-dbs/start-replset.sh
+++ testing-dbs/start-replset.sh
@@ -10,6 +10,10 @@ echo "Using MONGO_BIN=${MONGO_BIN}"
 #source ./mongodb-enterprise-${FLAVOR}-dependency-install.sh
 
 sudo killall mongod
+
+echo "Pausing to let mongod's shutdown"
+sleep 4
+
 sudo rm -rf /data/z-db-{1,2,3}
 sudo mkdir -p /data/z-db-{1,2,3}
 sudo chown linux1:linux1 /data/z-db-{1,2,3}
@@ -18,9 +22,12 @@ ${MONGO_BIN}/mongod -f ${CONF_DIR}/mongod-${FLAVOR}-one.conf
 ${MONGO_BIN}/mongod -f ${CONF_DIR}/mongod-${FLAVOR}-two.conf
 ${MONGO_BIN}/mongod -f ${CONF_DIR}/mongod-${FLAVOR}-three.conf
 
-${MONGO_BIN}/mongo --port 27000 --eval 'rs.initiate()'
+HOSTNAME=`hostname -f`
+echo "Using HOSTNAME=${HOSTNAME}"
+sleep 3
+${MONGO_BIN}/mongo --host ${HOSTNAME} --port 27000 --eval 'rs.initiate()'
 sleep 2
-${MONGO_BIN}/mongo --port 27000 --eval 'rs.add(hostname()+":27010")'
-${MONGO_BIN}/mongo --port 27000 --eval 'rs.add(hostname()+":27100")'
+${MONGO_BIN}/mongo --host ${HOSTNAME} --port 27000 --eval 'rs.add(hostname()+":27010")'
+${MONGO_BIN}/mongo --host ${HOSTNAME} --port 27000 --eval 'rs.add(hostname()+":27100")'
 
 
